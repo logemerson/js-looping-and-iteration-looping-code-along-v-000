0e0fe7fd7ab18142e87f836e347cf375e7b7ab51
Logan Emerson <logemerson@gmail.com>
2019-06-26 19:11:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function writeCards(namesArray) {
   for (let i = 0; i < namesArray.length; i++) {
-    console.log([`Thanks ${namesArray[i]}!`, `Thank you ${namesArray[i]}`, `Thanks so much ${namesArray[i]}`]);
+    return [`Thanks ${namesArray[i]}!`, `Thank you ${namesArray[i]}`, `Thanks so much ${namesArray[i]}`];
   }
 }
