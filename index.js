ed1afea30f7005584f713ad7cdd59d1849885127
Logan Emerson <logemerson@gmail.com>
2019-06-26 19:18:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function writeCards(namesArray) {
   let message = [];
   
   for (let i=0; i < namesArray.length; i++){
-    message.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);s
+    message.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);
   }
   return message;
 }
